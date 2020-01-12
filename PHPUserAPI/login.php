a384e458d2e02a15f152c379d640ab756c309404
calganaygun <calganaygun@hacettepe.edu.tr>
2020-01-12 14:02:19 +0300

    Login işlemleri için REST API

---
+++ PHPUserAPI/login.php
@@ -0,0 +1,25 @@
+<?
+//12 Ocak 2020 HCTP180 Çalgan Aygün
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+require_once('database.php');
+
+$phone =  $_POST["phone"]; //Gelen telefon numarasını aldık.
+$pass =  $_POST["pass"]; //SHA512 olrak gelen şifreyi aldık.
+$time = time(); //UNIX Zaman Damgasını aldık.
+$tokenKillingTimeStamp = time()+3500000; //Token'in son kullanım zmanını belirledik.
+
+$query = $db->query("SELECT * FROM users WHERE phone = '{$phone}' AND pass = '{$pass}'")->fetch(PDO::FETCH_ASSOC);
+if ($query){
+    $token = hash('sha256', $phone."ODTUIEE<3".$time."<3".$pass."HCTP180"); //User Token oluşturduk.
+    $returnArray = $query; //query Array'ını kopyaladık.
+    unset($returnArray["pass"]); // Kopyalanmış Array'dan şifre hash'ini çıkardık.
+    $returnArray["token"] = $token; // Array'e toke'i ekledik.
+	echo json_encode($returnArray)."\n"; //Json olarak kodlayıp ekrana bastık.
+	$query = $db->prepare("INSERT INTO `tokens` (`id`, `token`, `userid`, `lasttime`) VALUES (NULL, ?, ?, ?)");
+	$insert = $query->execute(array($token, $returnArray["id"], $tokenKillingTimeStamp));
+}else{
+	http_response_code(400);
+}
+$db = null;
+
