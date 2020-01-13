cbd23f8b1819ce1925ca1700532b1695a4297e72
calganaygun <calganaygun@hacettepe.edu.tr>
2020-01-13 09:16:49 +0300

    Timezone Istanbul olarak ayarlandı.

--- User-Backend-PHP/database.php
+++ User-Backend-PHP/database.php
@@ -2,6 +2,7 @@
 /**
 PDO ile güvenli bir şekilde veritabanına bağlanıldı ve bağlantı db değişkeni olarak anılacak.
 **/
+date_default_timezone_set('Europe/Istanbul');
 try {
      $db = new PDO("mysql:host=localhost;dbname=db_name", "db_username", "dbpass");
 } catch ( PDOException $e ){
